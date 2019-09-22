4b363fe518ed44a8dc0fbfb1ae0e7192f878273e
Apollo IT <p987415@gmail.com>
2019-09-22 16:15:59 +0000

    Made 2FA to optional.

--- app/controllers/private/invests_controller.rb
+++ app/controllers/private/invests_controller.rb
@@ -3,7 +3,8 @@ module Private
 
     before_action :auth_activated!
     before_action :auth_verified!
-    before_action :two_factor_activated!
+    # before_action :two_factor_activated!
+    before_action :two_factor_auth_passed!
 
     def index
       render json: current_user.invests.map(&:for_notify)
@@ -12,15 +13,15 @@ module Private
     def create
       @invest = current_user.invests.new invest_params
 
-      if two_factor_auth_verified?
+      # if two_factor_auth_verified?
         if @invest.save
           render nothing: true
         else
           render text: @invest.errors.full_messages.join(', '), status: 403
         end
-      else
-        render text: I18n.t('private.withdraws.create.two_factors_error'), status: 403
-      end
+      # else
+      #   render text: I18n.t('private.withdraws.create.two_factors_error'), status: 403
+      # end
     end
 
     private
--- app/controllers/private/point_exchanges_controller.rb
+++ app/controllers/private/point_exchanges_controller.rb
@@ -3,7 +3,8 @@ module Private
 
     before_action :auth_activated!
     before_action :auth_verified!
-    before_action :two_factor_activated!
+    # before_action :two_factor_activated!
+    before_action :two_factor_auth_passed!
 
     def index
       render json: current_user.point_exchanges.map(&:for_notify)
@@ -12,16 +13,16 @@ module Private
     def create
       @point_exchange = current_user.point_exchanges.new(point_exchange_params)
 
-      if two_factor_auth_verified?
+      # if two_factor_auth_verified?
         if @point_exchange.save
           @point_exchange.submit!
           render nothing: true
         else
           render text: @point_exchange.errors.full_messages.join(', '), status: 403
         end
-      else
-        render text: I18n.t('private.withdraws.create.two_factors_error'), status: 403
-      end
+      # else
+      #   render text: I18n.t('private.withdraws.create.two_factors_error'), status: 403
+      # end
     end
 
     private
