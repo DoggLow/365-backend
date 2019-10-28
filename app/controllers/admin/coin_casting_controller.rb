317110a4f09ad7308b60ce4053182e9a405669f0
YR199111RY <YR199111RY@protonmail.com>
2019-10-28 14:40:07 +0900

    feat: Add cc-admin-step2

--- app/controllers/admin/coin_casting_controller.rb
+++ app/controllers/admin/coin_casting_controller.rb
@@ -12,36 +12,50 @@ module Admin
     end
 
     def pool_history
+      member_id = params[:id]
+      user ||= Member.current = Member.enabled.where(id: member_id).first
       currency = 'pld'
       @data = []
-      @pool = current_user.fetch_pool(currency)
-      if @pool.present?
-        @data = (@pool.castings.done + @pool.pool_deposits).sort_by { |t| -t.created_at.to_i }
+      pool = user.fetch_pool(currency)
+      if pool.present?
+        @data = (pool.castings.done + pool.pool_deposits).sort_by { |t| -t.created_at.to_i }
       end
       @data = Kaminari.paginate_array(@data).page(params[:page]).per(20)
     end
 
     def accounts
-      @accounts = current_user.accounts.select { |account| account.currency != 'pld' && account.currency_obj.coin? }
+      member_id = params[:id]
+      user ||= Member.current = Member.enabled.where(id: member_id).first
+      @accounts = user.accounts.select { |account| account.currency != 'pld' && account.currency_obj.coin? }
     end
 
     def dashboard
+      member_id = params[:id]
+      user ||= Member.current = Member.enabled.where(id: member_id).first
       currency = 'pld'
-      pool = current_user.fetch_pool(currency)
+      pool = user.fetch_pool(currency)
       @wallet_balance = 0
       @cc_balance = 0
-      if @pool.present?
-        @wallet_balance = current_user.get_account(currency).balance
+      @other_balance = 0
+      @level = 0
+      @exp = 0
+      @exp_to_up = 0
+      if pool.present?
+        @wallet_balance = user.get_account(currency).balance
         @cc_balance = pool.castings.sum(:distribution)
+        @other_balance = pool.balance - @cc_balance
+        @level = user.cc_level
+        @exp = user.exp
+        @exp_to_up = CcLevel.to_up(user.exp)
       end
-      @other_balance = pool.balance - @cc_balance
+
       @info = {
           wallet_balance: @wallet_balance,
           cc_balance: @cc_balance,
           other_balance: @other_balance,
-          exp: current_user.exp,
-          level: current_user.cc_level,
-          exp_to_up: CcLevel.to_up(current_user.exp)
+          exp: user.exp,
+          level: user.cc_level,
+          exp_to_up: CcLevel.to_up(user.exp)
       }
     end
   end
